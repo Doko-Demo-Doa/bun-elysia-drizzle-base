909dc4771e36d4e3d31e5f95fe86506d842812e5
Quan Pham <doraemonfanclub@gmail.com>
2024-08-12 00:31:59 +0700

    Update schema

--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -1,15 +1,19 @@
 // Add necessary tables to this file.
 // They will automatically get picked up during deploy migrate step.
 
-// 👇See below for an example of a `users` table
+// 👇See below for an example of a `wallpapers` table
 
-// import { InferInsertModel, InferSelectModel } from 'drizzle-orm';
-// import { pgTable, uuid, varchar } from 'drizzle-orm/pg-core';
-//
-// export const users = pgTable('users', {
-//   id: uuid('id').defaultRandom().primaryKey(),
-//   name: varchar('name', { length: 256 }).notNull(),
-// });
-//
-// export type User = InferSelectModel<typeof users>;
-// export type NewUser = InferInsertModel<typeof users>;
+import { InferInsertModel, InferSelectModel } from "drizzle-orm";
+import { pgTable, uuid, smallint, varchar, date } from "drizzle-orm/pg-core";
+
+export const wallpapers = pgTable("wallpapers", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  fileName: varchar("file_name", { length: 2048 }).notNull(),
+  url: varchar("url", { length: 2048 }).notNull(),
+  isMobile: smallint("is_mobile").notNull(), // 0: false, 1: true
+  createdAt: date("created_at", { mode: "string" }).notNull(),
+  updatedAt: date("updated_at", { mode: "string" }),
+});
+
+export type Wallpaper = InferSelectModel<typeof wallpapers>;
+export type NewWallpaper = InferInsertModel<typeof wallpapers>;
