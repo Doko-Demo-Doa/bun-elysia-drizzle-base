6808b62e7a9abe99b761339a9e402b6a905dba18
Quan Pham <doraemonfanclub@gmail.com>
2024-08-12 00:53:41 +0700

    Add basic crons

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
 import { Elysia } from "elysia";
-import { swagger } from '@elysiajs/swagger'
+import { swagger } from "@elysiajs/swagger";
+import { cron } from "@elysiajs/cron";
 
 const app = new Elysia()
   .use(swagger())
-  .group('/api', (app) =>
-    app.get('/', () => {
-      return "Hello World!"
+  .group("/api", (app) =>
+    app.get("/", () => {
+      return "Hello World!";
+    })
+  )
+  .use(
+    cron({
+      name: "heartbeat",
+      pattern: "0 * * * *",
+      run() {
+        console.log("Heartbeat");
+      },
     })
   )
   .listen(process.env.PORT ?? 3000);
